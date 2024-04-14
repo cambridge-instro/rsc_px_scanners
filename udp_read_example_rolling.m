15c6e343784f3c74d489d7da31d09129fc44a66b
cambridge-instro <info@cambridge-instrumentation.co.uk>
2024-04-14 22:14:04 +0100

    Extra comments

--- udp_read_example_rolling.m
+++ udp_read_example_rolling.m
@@ -1,38 +1,53 @@
 
-%%
+%
+% Simple example to read a CI pressure scanner and plot the data on a
+% roling timebase
+%
+% Cambridge Instrumentataion, '23
+%
 
 clear all
 clc
 close all
 
+% Initialise the scanner using the port set in the web interface in setup
+% mode
 u4008 = scannerInit(4008);
+
+% Comment out the following "ZERO" command if not needed
 scannerZERO(u4008)
 
-f = 33; %approx sample rate
-T = 3; % width of plot in seconds
-N = T*f; % estimated number of samples in first window
-px_ = zeros(N,16)*NaN; % empty buffer
-t = linspace(0,T,N); % approx time vector to fill with real timestamps
+f = 33;                 % approx sample rate
+T = 3;                  % width of plot in seconds
+N = T*f;                % estimated number of samples in first window
+px_ = zeros(N,16)*NaN;  % empty buffer
+t = linspace(0,T,N);    % approx time vector to fill with real timestamps
 
 i = 1;
 
 while true
     
+    % flush the udp buffer to get latest data
     u4008.flush
     
+    % start the timebase
     if i == 1
         t0 = cputime;
     end
     
     if i > N
+        
         px_ = circshift(px_,-1);
+        
+        % get the pressure data
         px_(N,:) = scannerRead(u4008);
         
         t = circshift(t,-1);
         t(N) = cputime - t0;
         
-        
     else
+        
+        % get the pressure data
         px_(i,:) = scannerRead(u4008);
         t(i) = cputime - t0;
         
@@ -40,6 +55,7 @@ while true
     
     
     if mod(i,3) == 0
+        
         h2 = plot(t,px_);
         
         if i > N
@@ -54,34 +70,45 @@ while true
     
 end
 
-
-
-clear u4008
+%
+% helper functions to keep things tidy
+%
 
 function u = scannerInit(port)
 
-u = udpport("datagram",'LocalPort',port);
-u.flush
+    % start a udp object at the port setup with the webtool
+    u = udpport("datagram",'LocalPort',port);
+    u.flush
 
 end
 
 function scannerZERO(u)
 
-data = read(u,1,'string');
-write(u,'ZERO','string',data.SenderAddress,data.SenderPort)
+    % grab the listening port and IP address ready to send the "ZERO"
+    % command
+    data = read(u,1,'string');
+    
+    
+    % white a "ZERO" command, make sure the wind is off
+    write(u,'ZERO','string',data.SenderAddress,data.SenderPort)
 
 end
 
 function px = scannerRead(u)
 
-data = read(u,1,'string');
-px_tmp = split(data.Data,',');
-px = zeros(1,16);
+    % read the data from the udp pbject 
+    data = read(u,1,'string');
+    
+    px_tmp = split(data.Data,',');
+    
+    px = zeros(1,16);
 
-for i = 1:16
-    px(i) = str2double(px_tmp(i));
-end
-
+    for i = 1:16
+        
+        % convert from strings to double
+        px(i) = str2double(px_tmp(i));
+    
+    end
 
 
 end
